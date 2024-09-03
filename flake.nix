83a312c15c6fe84711f498ff15284e43579f9718
Ari Lotter <arilotter@gmail.com>
2024-09-03 17:13:22 -0400

    add libtorch 2.4.0 to nix flake

--- flake.nix
+++ flake.nix
@@ -24,28 +24,45 @@
         pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [(import inputs.rust-overlay)];
+
+          config.allowUnfree = true;
+          config.cudaSupport = true;
+          config.cudaVersion = "12";
         };
       in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-
-          buildInputs = with pkgs;
-            [
-              (rust-bin.stable.latest.default.override {
-                extensions = ["rust-src"];
-                targets = ["x86_64-unknown-linux-musl"];
-              })
-              alejandra # nix fmtter
-
-              # deps
-              openssl
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-              pkgs.linuxPackages_latest.perf
+        devShells.default = let
+          torch = pkgs.libtorch-bin.dev.overrideAttrs (old: {
+            version = "2.4.0";
+            src = pkgs.fetchzip {
+              name = "libtorch-cxx11-abi-shared-with-deps-2.4.0-cu121.zip";
+              url = "https://download.pytorch.org/libtorch/cu121/libtorch-cxx11-abi-shared-with-deps-2.4.0%2Bcu121.zip";
+              hash = "sha256-5HfCJ970GY3iqTKQD0GN0+FOVzCvBmZb0qXxcToTBHE=";
+            };
+          });
+        in
+          pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
             ];
-        };
+
+            buildInputs = with pkgs;
+              [
+                torch
+                (rust-bin.stable.latest.default.override {
+                  extensions = ["rust-src"];
+                  targets = ["x86_64-unknown-linux-musl"];
+                })
+                alejandra # nix fmtter
+
+                # deps
+                openssl
+              ]
+              ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+                pkgs.linuxPackages_latest.perf
+              ];
+
+            LIBTORCH = torch;
+          };
       };
     };
 }
