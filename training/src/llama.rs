503641e37f6a0b43f0fd866d7e404b1a411fdce4
emozilla <emozilla@nousresearch.com>
2024-09-10 21:51:56 -0400

    llama hf work

--- training/src/llama.rs
+++ training/src/llama.rs
@@ -1,24 +1,58 @@
 use tch::nn::{self, Module};
 use tch::{Device, Kind, Tensor};
 
+#[derive(Debug, Clone, serde::Deserialize, Default)]
+pub enum Llama3RopeType {
+    #[serde(rename = "llama3")]
+    Llama3,
+    #[default]
+    #[serde(rename = "default")]
+    Default,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, Default)]
+pub struct Llama3RopeConfig {
+    pub factor: f32,
+    pub low_freq_factor: f32,
+    pub high_freq_factor: f32,
+    pub original_max_position_embeddings: usize,
+    pub rope_type: Llama3RopeType,
+}
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(untagged)]
+pub enum LlamaEosToks {
+    Single(u32),
+    Multiple(Vec<u32>),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
 pub struct Config {
+    pub hidden_size: usize,
+    pub intermediate_size: usize,
     pub vocab_size: usize,
-    pub n_layer: usize,
-    pub n_head: usize,
-    pub n_embd: usize,
-    pub seq_len: usize,
+    pub num_hidden_layers: usize,
+    pub num_attention_heads: usize,
+    pub num_key_value_heads: usize,
+    pub rms_norm_eps: f64,
+    pub rope_theta: f32,
+    pub bos_token_id: Option<u32>,
+    pub eos_token_id: Option<LlamaEosToks>,
+    pub rope_scaling: Option<Llama3RopeConfig>,
+    pub max_position_embeddings: usize,
 }
 
 #[derive(Debug)]
 struct RmsNorm {
     scale: Tensor,
     size: i64,
+    eps: f64,
 }
 
 impl RmsNorm {
-    fn new(vs: nn::Path, size: i64) -> Self {
+    fn new(vs: nn::Path, size: i64, eps: f64) -> Self {
         let scale = vs.zeros("scale", &[size]);
-        Self { scale, size }
+        Self { scale, size, eps }
     }
 }
 
@@ -26,7 +60,7 @@ impl Module for RmsNorm {
     fn forward(&self, xs: &Tensor) -> Tensor {
         let kind = xs.kind();
         let norm_xs = (xs * xs).mean_dim(-1, true, Kind::Float);
-        let xs_normed = xs * (norm_xs + 1e-5).rsqrt();
+        let xs_normed = xs * (norm_xs + self.eps).rsqrt();
         let scale = self.scale.reshape([1, 1, self.size]);
         scale * xs_normed.to_kind(kind)
     }
@@ -40,16 +74,14 @@ struct Mlp {
 }
 
 impl Mlp {
-    fn new(vs: nn::Path, n_embd: i64) -> Self {
-        let n_hidden = 8 * n_embd / 3;
-        let n_hidden = (n_hidden - 1) / 256 * 256 + 256;
+    fn new(vs: nn::Path, n_embd: i64, n_hidden: i64) -> Self {
         let c = nn::LinearConfig {
             bias: false,
             ..Default::default()
         };
-        let c_fc1 = nn::linear(&vs / "c_fc1", n_embd, n_hidden, c);
-        let c_fc2 = nn::linear(&vs / "c_fc2", n_embd, n_hidden, c);
-        let c_proj = nn::linear(&vs / "c_proj", n_hidden, n_embd, c);
+        let c_fc1 = nn::linear(&vs / "gate_proj", n_embd, n_hidden, c);
+        let c_fc2 = nn::linear(&vs / "up_proj", n_embd, n_hidden, c);
+        let c_proj = nn::linear(&vs / "down_proj", n_hidden, n_embd, c);
         Self {
             c_fc1,
             c_fc2,
@@ -65,27 +97,41 @@ impl Module for Mlp {
     }
 }
 
+#[allow(dead_code)]
 #[derive(Debug)]
 struct CausalSelfAttention {
-    c_attn: nn::Linear,
-    c_proj: nn::Linear,
+    q_proj: nn::Linear,
+    k_proj: nn::Linear,
+    v_proj: nn::Linear,
+    o_proj: nn::Linear,
     n_head: i64,
+    n_kvhead: i64,
     n_embd: i64,
+    head_dim: i64,
     device: Device,
 }
 
 impl CausalSelfAttention {
-    fn new(vs: nn::Path, n_head: i64, n_embd: i64) -> Self {
+    fn new(vs: nn::Path, n_head: i64, n_kvheads: i64, n_embd: i64) -> Self {
         let c = nn::LinearConfig {
             bias: false,
             ..Default::default()
         };
-        let c_attn = nn::linear(&vs / "c_attn", n_embd, 3 * n_embd, c);
-        let c_proj = nn::linear(&vs / "c_proj", n_embd, n_embd, c);
+        let head_dim = n_embd / n_head;
+        let size_q = head_dim * n_head;
+        let size_kv = head_dim * n_kvheads;
+        let q_proj = nn::linear(&vs / "q_proj", n_embd, size_q, c);
+        let k_proj = nn::linear(&vs / "k_proj", n_embd, size_kv, c);
+        let v_proj = nn::linear(&vs / "n_proj", n_embd, size_kv, c);
+        let o_proj = nn::linear(&vs / "o_proj", size_q, n_embd, c);
         Self {
-            c_attn,
-            c_proj,
+            q_proj,
+            k_proj,
+            v_proj,
+            o_proj,
             n_head,
+            head_dim,
+            n_kvhead: n_kvheads,
             n_embd,
             device: vs.device(),
         }
@@ -115,17 +161,22 @@ impl CausalSelfAttention {
     fn forward(&self, x: &Tensor, freqs_cis: &Tensor) -> Tensor {
         let (b, t, c) = x.size3().unwrap();
         let kind = x.kind();
-        let qkv = self.c_attn.forward(x);
-        let n_embd = self.n_embd;
-        let q = qkv.slice(2, 0, n_embd, 1);
-        let k = qkv.slice(2, n_embd, 2 * n_embd, 1);
-        let v = qkv.slice(2, 2 * n_embd, 3 * n_embd, 1);
-        let target_dim = [b, t, self.n_head, c / self.n_head];
-        let k = k.reshape(target_dim).transpose(1, 2);
-        let q = q.reshape(target_dim).transpose(1, 2);
-        let v = v.reshape(target_dim).transpose(1, 2);
+        let q = self.q_proj.forward(x);
+        let k = self.k_proj.forward(x);
+        let v = self.v_proj.forward(x);
+        let k = k
+            .reshape([b, t, self.n_head, self.head_dim])
+            .transpose(1, 2);
+        let q = q
+            .reshape([b, t, self.n_kvhead, self.head_dim])
+            .transpose(1, 2);
+        let v = v
+            .reshape([b, t, self.n_kvhead, self.head_dim])
+            .transpose(1, 2);
         let q = self.apply_rotary_emb(&q, freqs_cis).to_kind(kind);
         let k = self.apply_rotary_emb(&k, freqs_cis).to_kind(kind);
+        let k = repeat_kv(k, self.n_head / self.n_kvhead);
+        let v = repeat_kv(v, self.n_head / self.n_kvhead);
         let mask = Tensor::ones([t, t], (kind, self.device))
             .tril(0)
             .reshape([1, 1, t, t]);
@@ -139,7 +190,7 @@ impl CausalSelfAttention {
         // let att = att.masked_fill(&mask.eq(0.), f64::NEG_INFINITY);
         // let y = att.softmax(-1, kind).matmul(&v);
         // let y = y.transpose(1, 2).reshape([b, t, c]);
-        self.c_proj.forward(&y)
+        self.o_proj.forward(&y)
     }
 }
 
@@ -153,11 +204,27 @@ struct Block {
 
 impl Block {
     fn new(vs: nn::Path, config: &Config) -> Self {
-        let rms_1 = RmsNorm::new(&vs / "rms_1", config.n_embd as i64);
-        let attn =
-            CausalSelfAttention::new(&vs / "attn", config.n_head as i64, config.n_embd as i64);
-        let rms_2 = RmsNorm::new(&vs / "rms_2", config.n_embd as i64);
-        let mlp = Mlp::new(&vs / "mlp", config.n_embd as i64);
+        let rms_1 = RmsNorm::new(
+            &vs / "input_layernorm",
+            config.hidden_size as i64,
+            config.rms_norm_eps as f64,
+        );
+        let attn = CausalSelfAttention::new(
+            &vs / "self_attn",
+            config.num_attention_heads as i64,
+            config.num_key_value_heads as i64,
+            config.hidden_size as i64,
+        );
+        let rms_2 = RmsNorm::new(
+            &vs / "rms_2",
+            config.hidden_size as i64,
+            config.rms_norm_eps as f64,
+        );
+        let mlp = Mlp::new(
+            &vs / "mlp",
+            config.hidden_size as i64,
+            config.intermediate_size as i64,
+        );
         Self {
             rms_1,
             attn,
@@ -187,22 +254,26 @@ impl Llama {
             bias: false,
             ..Default::default()
         };
+        let wte = nn::embedding(
+            &vs / "model" / "embed_tokens",
+            config.vocab_size as i64,
+            config.hidden_size as i64,
+            Default::default(),
+        );
         let lm_head = nn::linear(
             &vs / "lm_head",
-            config.n_embd as i64,
+            config.hidden_size as i64,
             config.vocab_size as i64,
             c,
         );
-        let wte = nn::embedding(
-            &vs / "transformer" / "wte",
-            config.vocab_size as i64,
-            config.n_embd as i64,
-            Default::default(),
+        let ln_f = RmsNorm::new(
+            &vs / "model" / "norm",
+            config.hidden_size as i64,
+            config.rms_norm_eps,
         );
-        let blocks = (0..config.n_layer)
-            .map(|i| Block::new(&vs / "transformer" / "h" / i, config))
+        let blocks = (0..config.num_hidden_layers)
+            .map(|i| Block::new(&vs / "model" / "layers" / i, config))
             .collect::<Vec<_>>();
-        let ln_f = RmsNorm::new(&vs / "transformer" / "ln_f", config.n_embd as i64);
         let freqs_cis = precompute_freqs_cis(config).to(vs.device());
         Self {
             wte,
@@ -224,17 +295,39 @@ impl Llama {
 }
 
 fn precompute_freqs_cis(config: &Config) -> Tensor {
-    let n_elem = config.n_embd / config.n_head;
+    let n_elem = config.hidden_size / config.num_attention_heads;
     let theta: Vec<_> = (0..n_elem)
         .step_by(2)
-        .map(|i| 1f32 / 10000f32.powf(i as f32 / n_elem as f32))
+        .map(|i| 1f32 / config.rope_theta.powf(i as f32 / n_elem as f32))
         .collect();
-    let arange: Vec<_> = (0..config.seq_len).map(|c| c as f32).collect();
+    let arange: Vec<_> = (0..config.max_position_embeddings)
+        .map(|c| c as f32)
+        .collect();
     let theta = Tensor::from_slice(&theta);
     let arange = Tensor::from_slice(&arange);
     let idx_theta = arange.outer(&theta);
-    let shape = [1, 1, config.seq_len as i64, n_elem as i64 / 2, 1];
+    let shape = [
+        1,
+        1,
+        config.max_position_embeddings as i64,
+        n_elem as i64 / 2,
+        1,
+    ];
     let idx_theta_cos = idx_theta.cos().reshape(shape);
     let idx_theta_sin = idx_theta.sin().reshape(shape);
     Tensor::cat(&[&idx_theta_cos, &idx_theta_sin], -1)
+}
+
+fn repeat_kv(xs: Tensor, n_rep: i64) -> Tensor {
+    if n_rep == 1 {
+        xs
+    } else {
+        let (b_sz, n_kv_head, seq_len, head_dim) = xs.size4().unwrap();
+        Tensor::cat(&vec![&xs; n_rep as usize], 2).reshape([
+            b_sz,
+            n_kv_head * n_rep,
+            seq_len,
+            head_dim,
+        ])
+    }
 }
--- training/src/main.rs
+++ training/src/main.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
+use crate::llama::{Config, Llama, Llama3RopeConfig, LlamaEosToks};
+use anyhow::{Error, Result};
 use batcher::Batcher;
-use llama::{Config, Llama};
 use psyche_client::download_repo_sync;
 use psyche_coordinator::model::HubRepo;
 use psyche_core::{CosineLR, LearningRateScheduler};
@@ -13,31 +13,31 @@ use tch::{Device, Tensor};
 mod batcher;
 mod llama;
 
-#[allow(dead_code)]
-const CONFIG_1_2B: Config = Config {
-    vocab_size: 32000,
-    n_layer: 16,
-    n_head: 16,
-    n_embd: 4096,
-    seq_len: 2048,
-};
-#[allow(dead_code)]
-const CONFIG_200M: Config = Config {
-    vocab_size: 32000,
-    n_layer: 12,
-    n_head: 12,
-    n_embd: 768,
-    seq_len: 2048,
-};
-#[allow(dead_code)]
-const CONFIG_100M: Config = Config {
-    vocab_size: 32000,
-    n_layer: 6,
-    n_head: 8,
-    n_embd: 512,
-    seq_len: 2048,
-};
-const CONFIG: &Config = &CONFIG_100M;
+// #[allow(dead_code)]
+// const CONFIG_1_2B: Config = Config {
+//     vocab_size: 32000,
+//     n_layer: 16,
+//     n_head: 16,
+//     n_embd: 4096,
+//     seq_len: 2048,
+// };
+// #[allow(dead_code)]
+// const CONFIG_200M: Config = Config {
+//     vocab_size: 32000,
+//     n_layer: 12,
+//     n_head: 12,
+//     n_embd: 768,
+//     seq_len: 2048,
+// };
+// #[allow(dead_code)]
+// const CONFIG_100M: Config = Config {
+//     vocab_size: 32000,
+//     n_layer: 6,
+//     n_head: 8,
+//     n_embd: 512,
+//     seq_len: 2048,
+// };
+// const CONFIG: &Config = &CONFIG_100M;
 const TOKEN_SIZE_IN_BYTES: usize = 2;
 const MICRO_BATCH_SIZE: usize = 8;
 const TOTAL_BATCH_SIZE: usize = 16;
@@ -55,6 +55,52 @@ const TOTAL_STEPS: usize = 25000;
 const MAX_GRAD_NORM: f64 = 1.0;
 const REPO_ID: &str = "emozilla/llama2-1.2b-init";
 
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct LlamaConfig {
+    pub hidden_size: usize,
+    pub intermediate_size: usize,
+    pub vocab_size: usize,
+    pub num_hidden_layers: usize,
+    pub num_attention_heads: usize,
+    pub num_key_value_heads: Option<usize>,
+    pub rms_norm_eps: f64,
+    #[serde(default = "default_rope")]
+    pub rope_theta: f32,
+    pub bos_token_id: Option<u32>,
+    pub eos_token_id: Option<LlamaEosToks>,
+    pub rope_scaling: Option<Llama3RopeConfig>,
+    pub max_position_embeddings: usize,
+}
+
+impl LlamaConfig {
+    pub fn num_key_value_heads(&self) -> usize {
+        self.num_key_value_heads.unwrap_or(self.num_attention_heads)
+    }
+}
+
+fn default_rope() -> f32 {
+    10_000.0
+}
+
+impl From<LlamaConfig> for Config {
+    fn from(value: LlamaConfig) -> Self {
+        Config {
+            hidden_size: value.hidden_size,
+            intermediate_size: value.intermediate_size,
+            vocab_size: value.vocab_size,
+            num_hidden_layers: value.num_hidden_layers,
+            num_attention_heads: value.num_attention_heads,
+            num_key_value_heads: value.num_key_value_heads(),
+            rms_norm_eps: value.rms_norm_eps,
+            rope_theta: value.rope_theta,
+            bos_token_id: value.bos_token_id,
+            eos_token_id: value.eos_token_id,
+            rope_scaling: value.rope_scaling,
+            max_position_embeddings: value.max_position_embeddings,
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let repo_files = download_repo_sync(
         HubRepo {
@@ -65,15 +111,22 @@ fn main() -> Result<()> {
         None,
         true,
     )?;
+    let config: LlamaConfig = serde_json::from_str(&String::from_utf8(std::fs::read(
+        repo_files
+            .iter()
+            .find(|x| x.ends_with("config.json"))
+            .ok_or(Error::msg("missing config.json"))?
+            .as_path(),
+    )?)?)?;
     let device = Device::Cuda(0);
     let dataset = LocalDataProvider::new_from_directory(
         "training/data",
         TOKEN_SIZE_IN_BYTES,
-        CONFIG.seq_len,
+        config.max_position_embeddings,
         rand::thread_rng().gen(),
     )?;
     let mut vs: nn::VarStore = nn::VarStore::new(device);
-    let model = Llama::new(vs.root(), CONFIG);
+    let model = Llama::new(vs.root(), &config.clone().into());
     vs.bfloat16();
     let iter = dataset.into_iter().map(|tokens| {
         let inputs = Tensor::from_slice(&tokens[..tokens.len() - 1]).to(device);
@@ -99,7 +152,7 @@ fn main() -> Result<()> {
         for _ in 0..GRAD_ACCUM_STEPS {
             let (inputs, targets) = batch_iter.next().unwrap()?;
             let logits = model.forward(&inputs);
-            let shift_logits = logits.view([-1i64, CONFIG.vocab_size as i64]);
+            let shift_logits = logits.view([-1i64, config.vocab_size as i64]);
             let shift_targets = targets.view(-1).to_kind(tch::Kind::Int64);
             let loss =
                 shift_logits.cross_entropy_for_logits(&shift_targets) / grad_accum_divisor.copy();
