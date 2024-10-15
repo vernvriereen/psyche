28b8b41b25a066196bca8e2314768672b7541a80
Ari Lotter <arilotter@gmail.com>
2024-10-15 18:38:45 -0400

    add local tests for data providers

---
+++ shared/data-provider/tests/local.rs
@@ -0,0 +1,47 @@
+use std::{fs::File, io::Write, path::PathBuf};
+
+use pretty_assertions::assert_eq;
+use psyche_data_provider::{LocalDataProvider, TokenSize, TokenizedDataProvider};
+use tokenizers::Tokenizer;
+use tokio::fs::read_to_string;
+fn test_path(path: &[&str]) -> PathBuf {
+    [env!("CARGO_MANIFEST_DIR"), "tests"]
+        .iter()
+        .chain(path)
+        .collect()
+}
+
+const SEED: [u8; 32] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30, 31, 32,
+];
+
+#[tokio::test]
+async fn loads_dolma_subset() {
+    let data_dir = test_path(&["resources", "dolma", "data"]);
+    let mut data_loader =
+        LocalDataProvider::new_from_directory(data_dir, TokenSize::TwoBytes, 2048, SEED).unwrap();
+    let samples = data_loader.get_samples(&[0, 1]).await.unwrap();
+
+    let tokenizer = Tokenizer::from_file(test_path(&["resources", "dolma", "tokenizer.json"]))
+        .expect("tokenizer json exists");
+    for (i, sample) in samples.into_iter().enumerate() {
+        let decoded_path = test_path(&["resources", "dolma", "decoded", &format!("{}.txt", i)]);
+
+        let expected = read_to_string(&decoded_path)
+            .await
+            .expect(&format!("no decoded file at {decoded_path:?}"));
+
+        let decoded = tokenizer
+            .decode(
+                &sample.into_iter().map(|x| x as u32).collect::<Vec<_>>(),
+                true,
+            )
+            .unwrap();
+
+        assert_eq!(
+            decoded, expected,
+            "sample {i} (left) doesn't match decoded reference (right) from file {decoded_path:?}"
+        );
+    }
+}
