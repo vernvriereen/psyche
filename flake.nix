2d5c270a9c2538067fda7adf7012406f2bcd5981
Ari Lotter <arilotter@gmail.com>
2024-10-30 11:36:50 -0400

    add docker image builder

--- flake.nix
+++ flake.nix
@@ -110,11 +110,33 @@
             done
           '';
       in rec {
-        packages = {
+        packages = rec {
           psyche-centralized-client = useHostGpuDrivers (buildPackage "psyche-centralized-client");
           psyche-centralized-server = buildPackage "psyche-centralized-server";
-          expand-distro = buildPackage "expand-distro";
           expand-distro = useHostGpuDrivers (buildPackage "expand-distro");
+          stream-docker-psyche-centralized-client = pkgs.dockerTools.streamLayeredImage {
+            name = "docker.io/nousresearch/psyche-centralized-client";
+            tag = "latest";
+            fromImage = pkgs.dockerTools.pullImage {
+              imageName = "nvidia/cuda";
+              imageDigest = "sha256:0f6bfcbf267e65123bcc2287e2153dedfc0f24772fb5ce84afe16ac4b2fada95"; # tag 12.4.1-base-ubuntu22.04
+              sha256 = "sha256-WFT1zPTFV9lAoNnT2jmKDB+rrbUSY9escmlO95pg4sA=";
+            };
+            config = {
+              # it's nice to have bash around for debugging.
+              contents = [pkgs.bash];
+              Env = [
+                "RUST_BACKTRACE=1"
+                "TUI=false"
+              ];
+              Entrypoint = [
+                "${psyche-centralized-client}/bin/psyche-centralized-client"
+                "train"
+                "--checkpoint-dir"
+                "./checkpoints"
+              ];
+            };
+          };
         };
 
         devShells.default = pkgs.mkShell {
