eb95ed3fa4346f54ca89bd72ee3813113268da26
Ari Lotter <arilotter@gmail.com>
2024-11-01 14:17:37 -0400

    update hf-hub & check models are reachable on startup

--- architectures/centralized/client/src/app.rs
+++ architectures/centralized/client/src/app.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 use std::time::Duration;
 
-use anyhow::{Error, Result};
+use anyhow::{bail, Error, Result};
+use hf_hub::Repo;
 use psyche_centralized_shared::{ClientId, ClientToServerMessage, ServerToClientMessage};
 use psyche_client::{CheckpointUploadInfo, Client, ClientTUI, ClientTUIState, WandBInfo, NC};
 use psyche_coordinator::{model, Coordinator, HealthChecks, Witness};
@@ -143,6 +144,22 @@ impl AppBuilder {
 
 impl App {
     pub async fn run(&mut self, mut p2p: NC) -> Result<()> {
+        // sanity checks
+        if let Some(CheckpointUploadInfo {
+            hub_repo,
+            hub_token,
+            ..
+        }) = &self.checkpoint_upload_info
+        {
+            let api = hf_hub::api::tokio::ApiBuilder::new()
+                .with_token(Some(hub_token.clone()))
+                .build()?;
+            let repo_api = api.repo(Repo::new(hub_repo.clone(), hf_hub::RepoType::Model));
+            if !repo_api.is_writable().await {
+                bail!("checkpoint upload repo {hub_repo} is not writable with the passed API key.")
+            }
+        }
+        
         self.server_conn
             .send(ClientToServerMessage::Join {
                 run_id: self.run_id.clone(),
