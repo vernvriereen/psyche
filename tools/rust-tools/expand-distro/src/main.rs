8bc765efd9a38cb7b51d433ed7ee3b0548f25a4a
Ari Lotter <arilotter@gmail.com>
2024-11-04 10:47:08 -0500

    fix expand_distro not matching devices for tensors

--- tools/rust-tools/expand-distro/src/main.rs
+++ tools/rust-tools/expand-distro/src/main.rs
@@ -20,8 +20,9 @@ fn main() -> anyhow::Result<()> {
     let distro_results_iter = distro_results_from_reader(io::stdin());
 
     for serialized_result in distro_results_iter {
-        let result: DistroResult = (&serialized_result?).try_into()?;
-
+        let mut result: DistroResult = (&serialized_result?).try_into()?;
+        result.sparse_idx = result.sparse_idx.to_device(target_device);
+        result.sparse_val = result.sparse_val.to_device(target_device);
         let decompressed = CompressDCT::decompress(
             &result.sparse_idx,
             &result.sparse_val,
