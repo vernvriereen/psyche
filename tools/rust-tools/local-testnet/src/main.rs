27caa63ffda690385ebf21f71c28dfb509c31246
Ari Lotter <arilotter@gmail.com>
2024-11-05 18:31:49 -0500

    add kill to test script

--- tools/rust-tools/local-testnet/src/main.rs
+++ tools/rust-tools/local-testnet/src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::{bail, Context, Result};
 use clap::Parser;
+use rand::seq::SliceRandom;
 use serde::Deserialize;
 use std::ffi::OsString;
-use std::os::unix::process::CommandExt;
+use std::net::TcpStream;
 use std::path::PathBuf;
 use std::process::Command;
-use std::thread::sleep;
 use std::time::Duration;
 
 #[derive(Parser, Debug)]
@@ -14,7 +14,7 @@ struct Args {
     /// Number of clients
     #[clap(long)]
     #[arg(value_parser = validate_num_clients)]
-    num_clients: u32,
+    num_clients: usize,
 
     /// Config directory path
     #[arg(value_parser = validate_config_path)]
@@ -27,21 +27,30 @@ struct Args {
 
     /// Server port
     #[clap(long)]
-    #[arg(default_value = "20000")]
+    #[arg(default_value_t = 20000)]
     server_port: u16,
 
     /// Enable TUI
     #[clap(long)]
-    #[arg(default_value = "true")]
-    tui: String,
+    #[arg(default_value_t = true)]
+    tui: bool,
 
     /// Force listed clients to use the same random data shuffle, causing them to train on duplicate data.
     #[clap(long, value_delimiter = ',', default_values_t = &[])]
     force_same_shuffle: Vec<usize>,
+
+    /// Kill N clients randomly every <RANDOM_KILL_INTERVAL> seconds
+    #[clap(long)]
+    random_kill_num: Option<usize>,
+
+    #[clap(long)]
+    #[arg(default_value_t = 120)]
+    /// Kill <RANDOM_KILL_NUM> clients randomly every N seconds
+    random_kill_interval: u64,
 }
 
-fn validate_num_clients(s: &str) -> Result<u32> {
-    let n: u32 = s
+fn validate_num_clients(s: &str) -> Result<usize> {
+    let n: usize = s
         .parse()
         .context("NUM_CLIENTS must be a positive integer")?;
     if n > 0 {
@@ -73,6 +82,14 @@ fn extract_run_id(state_path: &PathBuf) -> Result<String> {
 fn main() -> Result<()> {
     let args = Args::parse();
 
+    if let Some(n_kill) = args.random_kill_num {
+        if n_kill > args.num_clients {
+            bail!(
+                "You requested to kill {n_kill} clients randomly, but you only have {} clients.",
+                args.num_clients
+            );
+        }
+    }
     let state_path = args.config_path.join("state.toml");
     let data_path = args.config_path.join("data.toml");
 
@@ -160,8 +177,13 @@ fn main() -> Result<()> {
         .status()
         .expect("Failed to send server command");
 
-    println!("Waiting 10 seconds for server startup...");
-    sleep(Duration::from_secs(10));
+    println!("Waiting for server startup...");
+    loop {
+        if TcpStream::connect(format!("127.0.0.1:{}", args.server_port)).is_ok() {
+            println!("Server started!");
+            break;
+        }
+    }
 
     // Start nvtop
     Command::new("tmux")
@@ -175,45 +197,85 @@ fn main() -> Result<()> {
         .expect("Failed to start nvtop");
 
     // Start clients
-    for i in 2..=args.num_clients as usize + 1 {
-        let key_path = args
-            .config_path
-            .join("keys")
-            .join(format!("client-{}.key", i - 1));
-
-        Command::new("tmux")
-            .args(["select-pane", "-t", &i.to_string()])
-            .status()
-            .expect("Failed to select client pane");
-
-        let mut cmd: OsString = format!(
-            "RUST_BACKTRACE=1 cargo run -p psyche-centralized-client -- train --secret-key {} --run-id {} --server-addr localhost:{} --tui {}",
-            key_path.display(),
-            run_id,
-            args.server_port,
-            args.tui
-        ).into();
-
-        if let Some(dir) = &args.write_distro_data {
-            cmd.push(" --write-gradients-dir ");
-            cmd.push(dir);
-        }
-
-        if args.force_same_shuffle.contains(&(i - 1)) {
-            cmd.push(" --fixed-batch-shuffle 0000000000000000000000000000000000000000000000000000000000000001");
-        }
-
-        println!("starting client {i}: {cmd:?}");
-
-        Command::new("tmux")
-            .args([OsString::from("send-keys"), cmd, OsString::from("C-m")])
-            .status()
-            .expect("Failed to start client");
+    for i in 2..=args.num_clients + 1 {
+        start_client(&args, i, &run_id, true);
     }
 
     // Attach to tmux session
-    Command::new("tmux")
+    let mut tmux_session = Command::new("tmux")
         .args(["attach-session", "-t", "psyche"])
-        .exec();
+        .spawn()?;
+
+    if let Some(kill_num) = args.random_kill_num {
+        std::thread::sleep(Duration::from_secs(args.random_kill_interval));
+        let to_kill = {
+            let mut client_nums: Vec<usize> = (2..=args.num_clients + 1).collect();
+
+            client_nums.shuffle(&mut rand::thread_rng());
+
+            client_nums.truncate(kill_num);
+            client_nums
+        };
+        for kill in to_kill {
+            Command::new("tmux")
+                .args(["select-pane", "-t", &kill.to_string()])
+                .status()
+                .expect("Failed to select client pane");
+            // send ctrl-c
+            Command::new("tmux")
+                .args(["send-keys", "-t", &kill.to_string(), "C-c"])
+                .status()
+                .expect("Failed to kill client");
+            // restart client
+            start_client(&args, kill, &run_id, false);
+        }
+    }
+
+    let _ = tmux_session.wait(); // to prevent weird async tmux overlap with normal shell
+
+    // failsafe kill
+    Command::new("tmux")
+        .args(["kill-session", "-t", "psyche"])
+        .status()
+        .expect("Failed to kill tmux session");
+
     Ok(())
+}
+
+fn start_client(args: &Args, i: usize, run_id: &String, print: bool) {
+    let key_path = args
+        .config_path
+        .join("keys")
+        .join(format!("client-{}.key", i - 1));
+
+    Command::new("tmux")
+        .args(["select-pane", "-t", &i.to_string()])
+        .status()
+        .expect("Failed to select client pane");
+
+    let mut cmd: OsString = format!(
+        "RUST_BACKTRACE=1 cargo run -p psyche-centralized-client -- train --secret-key {} --run-id {} --server-addr localhost:{} --tui {}",
+        key_path.display(),
+        run_id,
+        args.server_port,
+        args.tui
+    ).into();
+
+    if let Some(dir) = &args.write_distro_data {
+        cmd.push(" --write-gradients-dir ");
+        cmd.push(dir);
+    }
+
+    if args.force_same_shuffle.contains(&(i - 1)) {
+        cmd.push(" --fixed-batch-shuffle 0000000000000000000000000000000000000000000000000000000000000001");
+    }
+
+    if print {
+        println!("starting client {i}: {cmd:?}");
+    }
+
+    Command::new("tmux")
+        .args([OsString::from("send-keys"), cmd, OsString::from("C-m")])
+        .status()
+        .expect("Failed to send server command");
 }
