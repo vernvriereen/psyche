57ce4ef429cfa99188935d80031f367d3f89e52b
Ari Lotter <arilotter@gmail.com>
2024-11-16 13:05:07 -0500

    update run id

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,7 @@ export const App = () => {
   const [fading, setFading] = useState<"loading" | "fading" | false>("loading");
   const [error, setError] = useState(false);
   const fetchWandbData = useCallback(() => {
-    getData("nous_research", "distro-live-test", "15b-100bt-6", 5000).then((data) => {
+    getData("nous_research", "distro-live-test", "15b-100bt-17", 5000).then((data) => {
       if (data) {
         setWandbRun(data);
       } else {
