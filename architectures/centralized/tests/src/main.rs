4373ffa5e539b3b0565a9bb0c50a5d47279aa690
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2024-11-27 18:54:36 +0000

    implement coordinator server actor for testing

--- architectures/centralized/tests/src/main.rs
+++ architectures/centralized/tests/src/main.rs
@@ -1,57 +1,108 @@
-fn main() {
-}
+fn main() {}
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
-
     use psyche_centralized_client::app::{AppBuilder, AppParams};
     use psyche_centralized_server::app::{App as ServerApp, DataServerInfo};
     use psyche_centralized_shared::ClientId;
     use psyche_client::BatchShuffleType;
-    use psyche_network::SecretKey;
-    use tokio::sync::Mutex;
-    use tokio::time::Duration;
     use psyche_coordinator::Coordinator;
-    use std::path::PathBuf;
     use psyche_data_provider::TokenSize;
+    use psyche_network::SecretKey;
+    use std::path::PathBuf;
+    use tokio::{
+        select,
+        sync::{
+            mpsc::{self, Receiver, Sender},
+            oneshot,
+        },
+        time::Duration,
+    };
     use tokio_util::sync::CancellationToken;
 
     const RUN_ID: &str = "test";
 
+    enum QueryMsg {
+        QueryClients { respond_to: oneshot::Sender<u32> },
+    }
 
+    struct CoordinatorServer {
+        inner: ServerApp,
+        query_chan_rx: Receiver<QueryMsg>,
+    }
+
+    impl CoordinatorServer {
+        pub async fn new(query_chan_rx: Receiver<QueryMsg>) -> Self {
+            let mut coordinator: Coordinator<ClientId> = Coordinator::default();
+            coordinator.run_id = RUN_ID.to_string();
+
+            let data_server_info = DataServerInfo {
+                dir: PathBuf::from("./"),
+                token_size: TokenSize::TwoBytes,
+                seq_len: 2048,
+                shuffle_seed: [1; 32],
+            };
+
+            let server = ServerApp::new(
+                false,
+                coordinator,
+                Some(data_server_info),
+                Some(1234),
+                Some(8080),
+                None,
+                None,
+                None,
+            )
+            .await
+            .unwrap();
+
+            Self {
+                inner: server,
+                query_chan_rx,
+            }
+        }
+
+        pub async fn handle_message(&mut self, msg: QueryMsg) {
+            match msg {
+                QueryMsg::QueryClients { respond_to } => {
+                    let clients_len = self.inner.get_pending_clients_len();
+                    let _ = respond_to.send(clients_len as u32).unwrap();
+                }
+            }
+        }
+
+        pub async fn run(&mut self) {
+            loop {
+                select! {
+                    res = self.inner.run() => res.unwrap(),
+                    Some(client_msg) = self.query_chan_rx.recv() => self.handle_message(client_msg).await
+                }
+            }
+        }
+    }
+
+    struct CoordinatorServerHandle {
+        sender: mpsc::Sender<QueryMsg>,
+    }
+
+    impl CoordinatorServerHandle {
+        pub async fn new() -> Self {
+            let (tx, rx) = mpsc::channel(8);
+            let mut server = CoordinatorServer::new(rx).await;
+            tokio::spawn(async move { server.run().await });
+            Self { sender: tx }
+        }
+
+        pub async fn get_clients_len(&self) -> u32 {
+            let (send, recv) = oneshot::channel();
+            let msg = QueryMsg::QueryClients { respond_to: send };
+            let _ = self.sender.send(msg).await;
+            recv.await.expect("Actor task has been killed")
+        }
+    }
 
     #[tokio::test]
     async fn connect_and_disconnect_nodes() {
-
-        let mut coordinator: Coordinator<ClientId> = Coordinator::default();
-
-        coordinator.run_id = RUN_ID.to_string();
-
-        let data_server_info = DataServerInfo {
-            dir: PathBuf::from("./"),
-            token_size: TokenSize::TwoBytes,
-            seq_len: 2048,
-            shuffle_seed: [1; 32],
-        };
-
-        let server = ServerApp::new(
-            false,
-            coordinator,
-            Some(data_server_info),
-            // p2p port
-            Some(1234),
-            Some(8080),
-            None,
-            None,
-            None,
-        )
-        .await
-        .unwrap();
-
-        let server = Arc::new(Mutex::new(server));
-        let server_clone = server.clone();
-        let server_task =
-            tokio::spawn(async move { server_clone.lock().await.run().await.unwrap() });
+        let server_handle = CoordinatorServerHandle::new().await;
 
         // Client
         let client_app_params = AppParams {
@@ -77,13 +128,11 @@ mod tests {
 
         let client_app_builder = AppBuilder::new(client_app_params);
 
-
         tokio::spawn(async { client_app_builder.run().await.unwrap() });
         tokio::time::sleep(Duration::from_secs(1)).await;
 
-        server_task.abort();
+        let num_clients = server_handle.get_clients_len().await;
 
-        assert_eq!(server.lock().await.get_pending_clients_len(), 1);
+        assert_eq!(num_clients, 1);
     }
 }
-
