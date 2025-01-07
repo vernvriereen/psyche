22a42076d8ce8d6b07e4735b72bda54a762ff52e
Pedro Fontana <pedro.fontana@lambdaclass.com>
2025-01-07 21:58:09 +0000

    allow Witness => WaitingForMembers state change

--- shared/client/src/state/steps.rs
+++ shared/client/src/state/steps.rs
@@ -548,6 +548,10 @@ impl<T: NodeIdentity, A: AuthenticatableIdentity + 'static> StepStateMachine<T, 
                 let trainers = cooldown.finish().await?;
                 ActiveStep::Warmup(self.warmup.start(trainers))
             }
+            (ActiveStep::Witness(witnessing), RunState::WaitingForMembers) => {
+                let trainers = witnessing.finish().await?.stop_evals().await?;
+                ActiveStep::Warmup(self.warmup.start(trainers))
+            }
             // stay in existing run state if there's no reason to change.
             (current_step, next_run_state) if current_step.allowed_in_run_state(next_run_state) => {
                 current_step
