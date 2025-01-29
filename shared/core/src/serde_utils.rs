a1e639828f667904fe81a561e1d4e0856e4ca607
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2025-01-29 18:51:44 +0000

    fix serialization bug & add some unit tests

--- shared/core/src/serde_utils.rs
+++ shared/core/src/serde_utils.rs
@@ -28,17 +28,17 @@ where
 }
 
 pub fn serde_serialize_optional_string<S, const N: usize>(
-    run_id: &Option<[u8; N]>,
+    str_bytes: &Option<[u8; N]>,
     serializer: S,
 ) -> std::result::Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    if let Some(run_id) = run_id {
+    if let Some(run_id) = str_bytes {
         let s = String::from_utf8_lossy(run_id)
             .trim_matches(char::from(0))
             .to_string();
-        serializer.serialize_str(&s)
+        serializer.serialize_some(&s)
     } else {
         serializer.serialize_none()
     }
@@ -83,4 +83,60 @@ where
     let len = std::cmp::min(vec.len(), N);
     arr[..len].copy_from_slice(&vec[..len]);
     Ok(arr)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+    struct MyOptionalStrStruct {
+        #[serde(
+            serialize_with = "serde_serialize_optional_string",
+            deserialize_with = "serde_deserialize_optional_string",
+            default
+        )]
+        field: Option<[u8; 64]>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+    struct MyStrStruct {
+        #[serde(
+            serialize_with = "serde_serialize_string",
+            deserialize_with = "serde_deserialize_string"
+        )]
+        field: [u8; 64],
+    }
+
+    #[test]
+    fn test_serialize_deserialize_optional_string_some() {
+        let my_struct = MyOptionalStrStruct {
+            field: Some([1u8; 64]),
+        };
+
+        let bytes = postcard::to_stdvec(&my_struct).unwrap();
+        let deserialized_struct: MyOptionalStrStruct = postcard::from_bytes(&bytes).unwrap();
+
+        assert_eq!(my_struct, deserialized_struct);
+    }
+
+    #[test]
+    fn test_serialize_deserialize_optional_string_none() {
+        let my_struct = MyOptionalStrStruct { field: None };
+
+        let bytes = postcard::to_stdvec(&my_struct).unwrap();
+        let deserialized_struct: MyOptionalStrStruct = postcard::from_bytes(&bytes).unwrap();
+
+        assert_eq!(my_struct, deserialized_struct);
+    }
+
+    #[test]
+    fn test_serialize_deserialize_string() {
+        let my_struct = MyStrStruct { field: [1u8; 64] };
+
+        let bytes = postcard::to_stdvec(&my_struct).unwrap();
+        let deserialized_struct: MyStrStruct = postcard::from_bytes(&bytes).unwrap();
+
+        assert_eq!(my_struct, deserialized_struct);
+    }
 }
