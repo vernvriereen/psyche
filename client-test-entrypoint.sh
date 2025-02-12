2c9d1770628a8462c18905465a75a4c95481be5a
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-02-12 16:41:09 +0000

    Update compose to run all services at once

--- client-test-entrypoint.sh
+++ client-test-entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-solana config set --url solana-text-validator:8899
-solana-keygen new --no-bip39-passphrase
-solana airdrop 1 $(solana-keygen pubkey ~/.config/solana/id.json)
+solana config set --url http://psyche-solana-test-validator:8899
+solana-keygen new --no-bip39-passphrase --force
 
-psyche-solana-client train --wallet-private-key-path "/root/.config/solana/id.json" --rpc "solana-test-validator:8899" --ws-rpc "solana-test-validator:8900" --run-id ${RUN_ID} --ticker --tui "false"
+solana airdrop 10 $(solana-keygen pubkey)
+psyche-solana-client train --wallet-private-key-path "/root/.config/solana/id.json" --rpc "http://psyche-solana-test-validator:8899" --ws-rpc "ws://psyche-solana-test-validator:8900" --run-id ${RUN_ID} --ticker --tui "false"
