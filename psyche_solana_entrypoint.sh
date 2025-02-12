20b51af0344235b8161de6ab744b8f48c298ab0e
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2025-02-12 16:06:59 +0000

    fine tune dockerfile and bash script for solana validator

--- psyche_solana_entrypoint.sh
+++ psyche_solana_entrypoint.sh
@@ -5,21 +5,13 @@ set -m
 
 RPC=${RPC:-"http://localhost:8899"}
 
-solana-keygen new --no-bip39-passphrase --force
-solana-keygen new --no-bip39-passphrase --outfile /solana_keys/client1_key.json --force
-solana-keygen new --no-bip39-passphrase --outfile /solana_keys/client2_key.json --force
-solana-keygen new --no-bip39-passphrase --outfile /solana_keys/client3_key.json --force
-
+solana-keygen new --no-bip39-passphrase
 solana config set --url localhost
 solana-test-validator -r &
 
-sleep 5
+sleep 3
 
-solana airdrop 5 $(solana-keygen pubkey /solana_keys/client1_key.json)
-solana airdrop 5 $(solana-keygen pubkey /solana_keys/client2_key.json)
-solana airdrop 5 $(solana-keygen pubkey /solana_keys/client3_key.json)
-
-pushd architectures/decentralized/solana-coordinator
+pushd /usr/local/solana-coordinator
 anchor deploy --provider.cluster ${RPC} -- --max-len 500000
 popd
 
