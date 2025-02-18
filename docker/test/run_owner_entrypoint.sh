9148770519b2998146e98c23dfeb8b2daa575b6a
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-02-18 04:19:42 +0000

    Reorg all docker related files

---
+++ docker/test/run_owner_entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+solana config set --url ${RPC}
+solana-keygen new --no-bip39-passphrase --force
+
+solana airdrop 10 $(solana-keygen pubkey)
+
+psyche-solana-client create-run --wallet-private-key-path "/root/.config/solana/id.json" --rpc ${RPC} --ws-rpc ${WS_RPC} --run-id "${RUN_ID}"
+psyche-solana-client update-config --wallet-private-key-path "/root/.config/solana/id.json" --rpc ${RPC} --ws-rpc ${WS_RPC} --run-id "${RUN_ID}" --config-path "/usr/local/config.toml"
+psyche-solana-client set-paused --wallet-private-key-path "/root/.config/solana/id.json" --rpc ${RPC} --ws-rpc ${WS_RPC} --run-id "${RUN_ID}" --resume
