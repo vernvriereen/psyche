bbbabaee1f6b9879a1dbdf14b2c93d734d88df0f
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-02-18 21:46:11 +0000

    Add new script to setup many clients assigning one gpu for each

---
+++ scripts/train-multiple-gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+num_clients=$1
+
+# Get the number of available GPUs
+num_gpus=$(nvidia-smi --query-gpu=index --format=csv,noheader | wc -l)
+
+for i in $(seq 1 "$num_clients"); do
+    gpu_id=$((i - 1))
+
+    if [ "$gpu_id" -ge "$num_gpus" ]; then
+        echo "Error: GPU index $gpu_id does not exist. Exiting." >&2
+        exit 1
+    fi
+
+    docker rm -f psyche-test-client-${i}
+    docker run -d \
+        --name psyche-test-client-${i} \
+        --gpus "device=$gpu_id" \
+        --env NVIDIA_DRIVER_CAPABILITIES=all \
+        --env-file config/client/.env.local \
+        --network test_psyche-test-network \
+        psyche-client
+    echo "Started psyche-test-client-${i} on GPU $gpu_id"
+done
