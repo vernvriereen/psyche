f616d17ef864f04e2dd3c984f80ea3df53165e6f
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2025-02-21 20:01:30 +0000

    fix docker compose file

--- docker/client_entrypoint.sh
+++ docker/client_entrypoint.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-psyche-solana-client train --wallet-private-key-path "/usr/local/id.json" --rpc ${RPC} --ws-rpc ${WS_RPC} --run-id ${RUN_ID} --ticker --tui "false"
+
+psyche-solana-client train \
+    --wallet-private-key-path "/usr/local/id.json" \
+    --rpc ${RPC} \
+    --ws-rpc ${WS_RPC} \
+    --run-id ${RUN_ID} \
+    --ticker \
+    --tui "false"
