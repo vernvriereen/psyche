fd0eb54c8d6d04fc260b340f984483b8262bac41
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-02-25 18:22:22 -0300

    First draft of p2p decentralized test

--- architectures/decentralized/testing/tests/integration_tests.rs
+++ architectures/decentralized/testing/tests/integration_tests.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use bollard::Docker;
 use e2e_testing::{
-    docker_setup::e2e_testing_setup,
+    docker_setup::{e2e_testing_setup, spawn_new_client},
     docker_watcher::{DockerWatcher, JsonFilter, Response},
 };
 use psyche_coordinator::RunState;
@@ -67,4 +67,68 @@ async fn one_client_three_epochs_run() {
             }
         }
     }
+}
+
+// Test p2p model sharing process
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn test_p2p() {
+    // set test variables
+    let run_id = "test".to_string();
+
+    // epochs the test will run
+    let num_of_epochs_to_run = 3;
+    let mut current_epoch = -1;
+    let mut last_epoch_loss = f64::MAX;
+
+    // initialize a Solana run with 1 client
+    let _cleanup = e2e_testing_setup(1);
+
+    spawn_new_client().await.unwrap();
+
+    tokio::signal::ctrl_c().await.unwrap();
+    // // initialize DockerWatcher
+    // let docker = Arc::new(Docker::connect_with_socket_defaults().unwrap());
+    // let (tx, mut rx) = mpsc::channel(100);
+    // let watcher = DockerWatcher::new(docker.clone(), tx);
+    // let _monitor_client_1 = watcher
+    //     .monitor_container(
+    //         "test-psyche-test-client-1",
+    //         vec![JsonFilter::StateChange, JsonFilter::Loss],
+    //     )
+    //     .unwrap();
+
+    // // initialize solana client to query the coordinator state
+    // let solana_client = SolanaTestClient::new(run_id).await;
+
+    // while let Some(response) = rx.recv().await {
+    //     match response {
+    //         Response::StateChange(timestamp, _client_1, old_state, new_state) => {
+    //             let coordinator_state = solana_client.get_run_state().await;
+    //             println!(
+    //                 "client: new_state: {}, old_state: {}, timestamp: {}",
+    //                 new_state, old_state, timestamp
+    //             );
+    //             // assert client and coordinator state synchronization
+    //             if new_state != RunState::WaitingForMembers.to_string() {
+    //                 assert_eq!(coordinator_state.to_string(), new_state.to_string());
+    //             }
+    //         }
+
+    //         Response::Loss(client, epoch, step, loss) => {
+    //             println!(
+    //                 "client: {:?}, epoch: {}, step: {}, Loss: {}",
+    //                 client, epoch, step, loss
+    //             );
+    //             // assert that the loss decreases each epoch
+    //             if epoch as i64 > current_epoch {
+    //                 current_epoch = epoch as i64;
+    //                 assert!(loss < last_epoch_loss);
+    //                 last_epoch_loss = loss;
+    //                 if epoch == num_of_epochs_to_run {
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
--- e2e_testing/src/docker_setup.rs
+++ e2e_testing/src/docker_setup.rs
@@ -1,4 +1,12 @@
-use std::process::{Command, Stdio};
+use bollard::models::DeviceRequest;
+use bollard::{
+    container::{Config, CreateContainerOptions, ListContainersOptions},
+    secret::{ContainerSummary, HostConfig},
+};
+use std::{
+    collections::HashMap,
+    process::{Command, Stdio},
+};
 use tokio::signal;
 
 use crate::docker_watcher::DockerWatcherError;
@@ -25,6 +33,74 @@ pub fn e2e_testing_setup(init_num_clients: usize) -> DockerTestCleanup {
     spawn_ctrl_c_task();
 
     DockerTestCleanup {}
+}
+
+pub async fn spawn_new_client() -> Result<(), DockerWatcherError> {
+    let docker_client = bollard::Docker::connect_with_socket_defaults().unwrap();
+    let containers: Vec<ContainerSummary> =
+        docker_client.list_containers::<String>(None).await.unwrap();
+    let container_names: Vec<String> = containers
+        .into_iter()
+        .filter_map(|cont| {
+            cont.names
+                .clone()
+                .unwrap_or_default()
+                .get(0)
+                .filter(|name| name.starts_with("/test-psyche-test-client"))
+                .cloned()
+        })
+        .collect();
+
+    // Define GPU capabilities
+    let device_request = DeviceRequest {
+        driver: Some("nvidia".to_string()),
+        count: Some(1),
+        capabilities: Some(vec![vec!["gpu".to_string()]]),
+        ..Default::default()
+    };
+
+    let network_name = "test_psyche-test-network"; // Replace with your actual network
+                                                   // Define host configuration
+    let host_config = HostConfig {
+        device_requests: Some(vec![device_request]),
+        extra_hosts: Some(vec!["host.docker.internal:host-gateway".to_string()]),
+        network_mode: Some(network_name.to_string()), // Attach container to the network
+        ..Default::default()
+    };
+
+    // Read environment variables from `.env` file
+    let a = std::env::current_dir();
+    println!("{}", a.unwrap().display());
+    let env_vars = std::fs::read_to_string("../../../config/client/.env.local")
+        .unwrap()
+        .lines()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let envs = vec![env_vars, vec!["NVIDIA_DRIVER_CAPABILITIES=all".to_string()]].concat();
+
+    let options = Some(CreateContainerOptions {
+        name: format!("test-psyche-test-client-{}", container_names.len() + 1),
+        platform: None,
+    });
+    let config = Config {
+        image: Some("psyche-client"),
+        env: Some(envs.iter().map(|s| s.as_str()).collect()),
+        host_config: Some(host_config),
+        ..Default::default()
+    };
+    docker_client
+        .create_container(options, config)
+        .await
+        .unwrap();
+    docker_client
+        .start_container::<String>(
+            &format!("test-psyche-test-client-{}", container_names.len() + 1),
+            None,
+        )
+        .await
+        .unwrap();
+    Ok(())
 }
 
 pub fn spawn_psyche_network(init_num_clients: usize) -> Result<(), DockerWatcherError> {
