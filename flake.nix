376c188cdd15ae8ea05a5d9cb1fb5bedc1993e58
Ari Lotter <arilotter@gmail.com>
2025-02-27 14:52:01 -0500

    cleanup: remove aarch64 from flake (doesn't work with cuda yet)

--- flake.nix
+++ flake.nix
@@ -32,7 +32,6 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
       ];
       imports = [
         (import ./nix/packages.nix {inherit self inputs;})
