df117c9280a6c0c9658159d4bc4e027882259497
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-03-09 20:21:37 +0900

    nit

--- docker/test/run_owner_entrypoint.sh
+++ docker/test/run_owner_entrypoint.sh
@@ -5,20 +5,20 @@ set -o errexit
 solana airdrop 10 "$(solana-keygen pubkey)" --url "${RPC}"
 
 psyche-solana-client create-run \
-    --wallet-private-key-path ${WALLET_FILE} \
+    --wallet-private-key-path "/root/.config/solana/id.json" \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}"
 
 psyche-solana-client update-config \
-    --wallet-private-key-path ${WALLET_FILE} \
+    --wallet-private-key-path "/root/.config/solana/id.json" \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}" \
     --config-path "/usr/local/config.toml"
 
 psyche-solana-client set-paused \
-    --wallet-private-key-path ${WALLET_FILE} \
+    --wallet-private-key-path "/root/.config/solana/id.json" \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}" \
