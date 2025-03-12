fd7932160a0b4164b7429fdf4228760a1e9136ce
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2025-03-12 15:15:00 -0300

    include solana authorizer program address check to script

--- scripts/coordinator-address-check.sh
+++ scripts/coordinator-address-check.sh
@@ -5,13 +5,22 @@ set -o pipefail
 
 cd architectures/decentralized/solana-coordinator
 coordinator_address=$(grep -r --include='*.rs' 'declare_id!(' . | sed -n 's/.*declare_id!(\([^)]*\)).*/\1/p')
+cd ../solana-authorizer
+authorizer_address=$(grep -r --include='*.rs' 'declare_id!(' . | sed -n 's/.*declare_id!(\([^)]*\)).*/\1/p')
 
 if [ -z "$coordinator_address" ]; then
-  echo "Error: No declare_id! macro found."
+  echo "Error: No declare_id! macro found for coordinator."
   exit 1
 fi
 
-echo -e "\n${coordinator_address} is the address of the coordinator program that will be used in the psyche test client binary.\n"
+if [ -z "$authorizer_address" ]; then
+  echo "Error: No declare_id! macro found for authorizer."
+  exit 1
+fi
+
+echo -e "\nCoordinator address: ${coordinator_address}\n"
+echo -e "Authorizer address: ${authorizer_address}\n"
+echo -e "These are the addresses that will be used in the Psyche client binary"
 read -p "Continue? [y/N] " answer
 
 if [[ "$answer" =~ ^[Yy]$ || -z "$answer" ]]; then
