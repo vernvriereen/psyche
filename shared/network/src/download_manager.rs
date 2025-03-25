e415dd17f56a034423b29077f653787dffbc93b3
emozilla <emozilla@nousresearch.com>
2025-03-25 10:02:08 -0700

    disable compression

--- shared/network/src/download_manager.rs
+++ shared/network/src/download_manager.rs
@@ -390,12 +390,13 @@ impl<D: Networkable + Send + 'static> DownloadManager<D> {
     ) -> Result<Option<DownloadManagerEvent<D>>> {
         match result {
             Ok(bytes) => {
-                let decompressed_bytes = {
-                    let mut decoder = ZlibDecoder::new(&bytes[..]);
-                    let mut result = Vec::new();
-                    decoder.read_to_end(&mut result)?;
-                    result
-                };
+                // let decompressed_bytes = {
+                //     let mut decoder = ZlibDecoder::new(&bytes[..]);
+                //     let mut result = Vec::new();
+                //     decoder.read_to_end(&mut result)?;
+                //     result
+                // };
+                let decompressed_bytes = bytes;
 
                 let decoded = postcard::from_bytes(&decompressed_bytes)?;
 
