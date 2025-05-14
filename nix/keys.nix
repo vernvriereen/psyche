2cd58904247673a46f5d564a9479010f0053a980
Ari Lotter <arilotter@gmail.com>
2025-05-14 15:37:25 -0400

    secrets: re-key with the new repo key

--- nix/keys.nix
+++ nix/keys.nix
@@ -3,8 +3,11 @@
 # some are used to decrypt secrets.
 # which key -> which secret is determined in secrets.nix.
 rec {
-  # from garnix, a unique ssh key for this repo
-  repoKey = "age1qjv2zfrgwdhc0pw3t2rqk0zv6zudwqjjdz8plmy9u2edyxm77crqr73uqj";
+  # from garnix, a unique ssh key for NousResearch/psyche
+  nousRepoKey = "age1qjv2zfrgwdhc0pw3t2rqk0zv6zudwqjjdz8plmy9u2edyxm77crqr73uqj";
+
+  # from garnix, a unique ssh key for PsycheFoundation/psyche
+  psycheRepoKey = "age13lxrsre5klvs777t6c748tnuskjx9g54wffc6at3hfhx2fj4ccts5ale7t";
 
   ariLunaKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIL+5IDeIKvYpQllVsU/soRu27KyPTA5FXvZM5Z8+ms7 arilotter@gmail.com";
   ariHermesKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKaPWTrDp1sp3NUXiM/JXKfivQQ6TLxMy7Fyaq59L7y arilotter@gmail.com";
@@ -15,5 +18,8 @@ rec {
 
   allDevKeys = ariKeys;
 
-  allKeys = [ repoKey ] ++ allDevKeys;
+  allKeys = [
+    nousRepoKey
+    psycheRepoKey
+  ] ++ allDevKeys;
 }
